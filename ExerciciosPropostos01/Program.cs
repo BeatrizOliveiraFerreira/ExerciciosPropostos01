5e2c0de35deb7195a56e0560a8c3a8d833cbd3e2
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 15:25:36 -0300

    Add project files.

---
+++ ExerciciosPropostos01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com o número");
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int total = valor1 + valor2;
+
+            Console.WriteLine($"SOMA = {total}");
+        }
+    }
+}
